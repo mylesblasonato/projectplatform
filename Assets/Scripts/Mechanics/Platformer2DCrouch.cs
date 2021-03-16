2345e5ce97edb7569e712cbb9a21a3e8c2e4550e
Myles <mylesblasonato988@hotmail.com>
2021-03-16 20:58:20 +1100

    fixed crouch jumping

--- Assets/Scripts/Mechanics/Platformer2DCrouch.cs
+++ Assets/Scripts/Mechanics/Platformer2DCrouch.cs
@@ -17,6 +17,7 @@ public class Platformer2DCrouch : MonoBehaviour
     RaycastHit2D _slope;
     bool _isSloping = false;
 
+    public float _crouchAxis = 0;
     public bool _isCrouching = false;
 
     void Start()
@@ -28,6 +29,25 @@ public class Platformer2DCrouch : MonoBehaviour
 
     void Update()
     {
+        SlopeRaycastAndCheck();
+        if (!_jumpMechanic._isGrounded) return;
+        CrouchDown();
+        Stand();
+    }
+
+    //
+
+    // Slows down the player when crouching
+    void ManualFriction()
+    {
+        if (_rb.velocity.x < 0)
+            _rb.velocity = new Vector2(_rb.velocity.x + 0.05f, 0);
+        if (_rb.velocity.x > 0)
+            _rb.velocity = new Vector2(_rb.velocity.x - 0.05f, 0);
+    }
+
+    void SlopeRaycastAndCheck()
+    {
         _slope = Physics2D.Raycast(
             Vector2.zero + new Vector2(transform.localPosition.x, transform.localPosition.y),
             new Vector2(0, -1),
@@ -35,46 +55,12 @@ public class Platformer2DCrouch : MonoBehaviour
             _slopeMask);
 
         if (_slope && !_isSloping)
-        {
             Slope();
-        }
 
         if (!_slope)
         {
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, 0);
             _isSloping = false;
-        }
-        
-
-        if (_jumpMechanic._isGrounded)
-        {
-            if (_isCrouching)
-            {
-                _boxColliderStand.enabled = false;
-                _boxColliderCrouch.enabled = true;
-                _playerSpriteTop.transform.position = _crouchPos.position;
-                _playerSpriteTop.transform.localRotation = _crouchPos.localRotation;
-                EventManager.Instance.Crouch();
-
-                if (_moveMechanic._isRunning)
-                {
-                    _rb.drag = _crouchDrag.Value;
-                }
-                else
-                {
-                    if (_rb.velocity.x < 0)
-                        _rb.velocity = new Vector2(_rb.velocity.x + 0.05f, 0);
-                    if (_rb.velocity.x > 0)
-                        _rb.velocity = new Vector2(_rb.velocity.x - 0.05f, 0);
-                }
-            }
-            else
-            {
-                _boxColliderStand.enabled = true;
-                _boxColliderCrouch.enabled = false;
-                _playerSpriteTop.transform.localPosition = Vector3.zero;
-                EventManager.Instance.Stand();
-            }
         }
     }
 
@@ -97,28 +83,47 @@ public class Platformer2DCrouch : MonoBehaviour
         if (_canCrouchDash && _moveMechanic._isRunning && _isCrouching && _moveMechanic._isMoving && Mathf.Abs(_rb.velocity.x) > _crouchDashSpeedThreshold.Value)
         {
             _rb.velocity = Vector2.zero;
-            _rb.AddForce(transform.right * _dashCrouchForce.Value * Time.deltaTime, ForceMode2D.Impulse);
+            _rb.AddForce(transform.right * _dashCrouchForce.Value, ForceMode2D.Impulse);
             _canCrouchDash = false;
         }
     }
 
+    public void Stand()
+    {
+        if (_isCrouching) return;
+        _boxColliderStand.enabled = true;
+        _boxColliderCrouch.enabled = false;
+        _playerSpriteTop.transform.localPosition = Vector3.zero;
+        EventManager.Instance.Stand();
+    }
+
+    public void CrouchDown()
+    {
+        if (!_isCrouching) return;
+        _boxColliderStand.enabled = false;
+        _boxColliderCrouch.enabled = true;
+        _playerSpriteTop.transform.position = _crouchPos.position;
+        _playerSpriteTop.transform.localRotation = _crouchPos.localRotation;
+        EventManager.Instance.Crouch();
+        if (_moveMechanic._isRunning)
+            _rb.drag = _crouchDrag.Value;
+        else
+            ManualFriction();
+    }
+
     public void Crouch(float isCrouching)
     {
+        _crouchAxis = isCrouching;
         _isCrouching = (isCrouching >= _crouchThreshold.Value);      
-
         if (!_isCrouching)
-        {
             _canCrouchDash = true;
-        }
     }
 
     #region HELPERS
     private void OnDrawGizmos()
     {
         if (_slope)
-        {
             Gizmos.DrawLine(_slope.point, _slope.normal + new Vector2(transform.position.x, transform.position.y));
-        }
     }
     #endregion
 }
--- Assets/Scripts/Mechanics/Platformer2DJump.cs
+++ Assets/Scripts/Mechanics/Platformer2DJump.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Platformer2DJump : MonoBehaviour
 {
-    [SerializeField] SoFloat _jumpForce, _gravity, _drag, _airDrag, _dragSensitivity, _fallMultiplier, _jumpTimer, _jumpCount, _inputDirection;
+    [SerializeField] SoFloat _jumpForce, _gravity, _drag, _airDrag, _dragSensitivity, _fallMultiplier, _jumpTimer, _jumpCount, _inputDirection, _cyoteTime, _crouchThreshold;
     [SerializeField] Transform _groundCheck;
     [SerializeField] float _groundCheckRadius;
     [SerializeField] LayerMask _groundMask;
 
     [HideInInspector] public bool _isJumping = false;
-    [HideInInspector] public bool _isGrounded = false;
+     public bool _isGrounded = false;
 
     Platformer2DCrouch _crouchMechanic;
     Rigidbody2D _rb;
@@ -32,11 +32,12 @@ public class Platformer2DJump : MonoBehaviour
 
     public void Jump(float isJumping)
     {
-        if (isJumping == 1 && !_crouchMechanic._isCrouching)
+        if (isJumping == 1)
         {
             if (_currentJumpCount < _jumpCount.Value)
             {
                 _isJumping = true;
+                _crouchMechanic._isCrouching = false;
                 EventManager.Instance.Jump();
                 Invoke("StopJump", _jumpTimer.Value);
             }
@@ -50,18 +51,18 @@ public class Platformer2DJump : MonoBehaviour
     void Jumping()
     {
         if (_isJumping)
-            _rb.AddForce(Vector2.up * _currentJumpForce, ForceMode2D.Impulse);          
+            _rb.AddForce(Vector2.up * _currentJumpForce, ForceMode2D.Impulse);
     }
 
     void StopJump() => _isJumping = false;
 
     void GroundCheck()
     {
-        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundMask);
-        if (_isGrounded && !_crouchMechanic._isCrouching)
-        {
+        Invoke("DelayedGroundCheck", 0.1f);
+        if (_isGrounded)
+        {          
             EventManager.Instance.Land();
-            _currentJumpCount = 0;            
+            _currentJumpCount = 0;
             if (Mathf.Abs(_rb.velocity.x) > _dragSensitivity.Value)
                 _rb.drag = _drag.Value;
             else
@@ -69,12 +70,20 @@ public class Platformer2DJump : MonoBehaviour
             _currentJumpForce = _jumpForce.Value;
         }
         else
-        {
+        {         
             _currentJumpCount++;
             _rb.drag = _drag.Value * _airDrag.Value;
             _rb.gravityScale = _gravity.Value;
+
+            if (_crouchMechanic._crouchAxis >= _crouchThreshold.Value)
+                _crouchMechanic._isCrouching = true;
         }
         _currentJumpForce -= _fallMultiplier.Value;
+    }
+
+    void DelayedGroundCheck()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheck.position, _groundCheckRadius, _groundMask);        
     }
 
     #region HELPERS
